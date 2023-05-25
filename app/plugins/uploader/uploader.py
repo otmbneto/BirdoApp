8153148e46b20b982ead29f45e96aabf35d95dff
otmbneto <otmbneto@protonmail.com>
2023-05-25 12:04:24 -0300

    code clean up

--- app/plugins/uploader/uploader.py
+++ app/plugins/uploader/uploader.py
@@ -7,11 +7,13 @@ from distutils.dir_util import copy_tree
 from PySide import QtCore, QtGui, QtUiTools
 import shutil
 import tempfile
+import copy
 
 curr_dir = os.path.dirname(os.path.realpath(__file__))
 ui_path = os.path.join(curr_dir, 'ui')
 birdo_app_root = os.path.dirname(os.path.dirname(os.path.dirname(curr_dir)))
 birdo_utils = os.path.join(birdo_app_root, 'app', 'utils')
+birdo_proj_utils = os.path.join(birdo_app_root, 'config', 'projects')
 
 global_icons = {
     "birdo_app": os.path.join(birdo_app_root, 'app', 'icons', 'birdoAPPLogo.ico'),
@@ -30,163 +32,6 @@ from app.utils.ffmpeg import compress_render
 from app.utils.system import get_short_path_name
 
 MessageBox = CreateMessageBox()
-
-
-class VideoItemWidget(QtGui.QGroupBox):
-
-    def __init__(self,file_path,episodes):
-
-        super(VideoItemWidget,self).__init__()
-
-        self.setMinimumHeight(50)
-        self.setMaximumHeight(50)
-        self.isMov = False
-        self.filename = file_path.split("/")[-1]
-        self.filepath = "/".join(file_path.split("/")[:-1]) + "/"
-
-        if file_path.endswith((".mov",".mp4")):
-        	self.createVideoWidget(file_path,episodes)
-        	self.isMov = True
-
-        '''
-        if file_path.endswith((".mov",".zip")):
-            self.createVideoWidget(file_path,statuses)
-        else:
-            self.createFileWidget(file_path)
-        '''
-
-        return
-
-    def isMovie(self):
-
-    	return self.isMov
-
-    def createFileWidget(self,filepath):
-
-        horizontal_layout = QtGui.QHBoxLayout()
-
-        item_label = QtGui.QLabel(self.filename)
-        item_font = QtGui.QFont("Arial", 12)
-        item_label.setFont(item_font)
-
-        self.delete_button = QtGui.QPushButton("X")
-        self.delete_button.clicked.connect(self.close)
-        self.delete_button.setMaximumWidth(25)
-
-        horizontal_layout.addWidget(item_label)
-        horizontal_layout.addWidget(self.delete_button)
-
-        self.setLayout(horizontal_layout)
-
-        return
-
-    def setStatuses(self,statuses):
-
-        for status in statuses:
-            self.status_box.addItem(statuses[status],status)
-
-
-    def createVideoWidget(self,filepath,episodes):
-
-        horizontal_layout = QtGui.QHBoxLayout()
-        item_label = QtGui.QLabel(self.filename)
-        item_font = QtGui.QFont("Arial", 12)
-        item_label.setFont(item_font)
-
-        self.episodes = QtGui.QComboBox()
-        self.episodes.addItems(episodes)
-
-        self.progress_bar = QtGui.QProgressBar()
-        self.progress_bar.setMinimum(0)
-        self.progress_bar.setMaximum(100)
-        self.progress_bar.setMinimumWidth(100)
-        self.progress_bar.setMaximumWidth(100)
-        self.progress_bar.setValue(0)
-
-        self.status_label = QtGui.QLabel("<font>Ready to go</font>")
-        self.status_label.setStyleSheet("QLabel { color : blue; }")
-        self.status_label.setFont(item_font)
-        self.status_label.setFrameStyle(QtGui.QFrame.Panel | QtGui.QFrame.Sunken)
-        self.status_label.setMinimumWidth(100)
-        self.status_label.setMaximumWidth(100)
-        self.status_label.setMinimumHeight(25)
-        self.status_label.setMaximumHeight(25)
-        
-        self.delete_button = QtGui.QPushButton("X")
-        self.delete_button.clicked.connect(self.close)
-        self.delete_button.setMaximumWidth(25)
-
-        horizontal_layout.addWidget(item_label)
-        horizontal_layout.addWidget(self.episodes)
-        horizontal_layout.addWidget(self.progress_bar)
-        horizontal_layout.addWidget(self.status_label)        
-        horizontal_layout.addWidget(self.delete_button)
-        horizontal_layout.addStretch()
-        self.setLayout(horizontal_layout)
-
-        return
-
-    def getProgress(self):
-        return self.progress_bar.value()
-
-    def setProgress(self,value):
-        
-        self.progress_bar.setValue(value)
-
-    def incrementProgress(self,increment):
-        value = self.getProgress()
-        self.setProgress(value + increment)
-        
-    def getStep(self):
-        return self.step_box.currentText()
-
-    def setStep(self,index):
-
-        self.step_box.setCurrentIndex(index)
-        
-    def getSgStatus(self):
-        
-        return self.status_box.currentText()
-
-    def setSgStatus(self,index):
-
-        self.status_box.setCurrentIndex(index)
-
-    def getFilepath(self):
-        return self.filepath
-
-    def getFilename(self):
-        return self.filename
-    
-    def getFullpath(self):
-        return self.filepath + self.filename
-    
-    def getStatus(self):
-        return self.status_label
-
-    def setStatus(self,text,color):
-        self.status_label.setText(text)
-        self.status_label.setStyleSheet("QLabel { color : " + color + "; }")
-
-    def getEpisode(self):
-    	return self.episodes.currentText()
-
-    def setEpisode(self,index):
-    	self.episodes.setCurrentIndex(index)
-
-    def setDone(self):
-
-        self.status_label.setText("Done")
-        self.status_label.setStyleSheet("QLabel { color : green; }")
-
-    def setError(self):
-
-        self.status_label.setText("ERROR")
-
-    def setEnable(self,value):
-
-        self.delete_button.setEnabled(value)
-
 
 class Uploader(QtGui.QMainWindow):
 
@@ -213,6 +58,20 @@ class Uploader(QtGui.QMainWindow):
 		self.resize(w, h)
 		self.setLogic()
 		self.setAcceptDrops(True)
+
+
+	def getTemplateItem(self,path,episodes):
+
+		template_item = None
+		ui_utils = os.path.join(birdo_proj_utils,self.project_data["prefix"],"UI_utils")
+		if os.path.exists(ui_utils):
+			ui = os.path.join(ui_utils,"uploaderItem.ui")
+			#script = os.path.join(ui_utils,"uploaderItem.py")
+			sys.path.append(ui_utils)
+			import uploaderItem as upi 
+			template_item = upi.uiItem(path,episodes)
+
+		return template_item
 
 	def getRoot(self):
 
@@ -301,57 +160,18 @@ class Uploader(QtGui.QMainWindow):
 
 		self.ui.cleanBtn.setEnabled(True)
 
-		progression = 100.0/len(self.listOfWidgets) if len(self.listOfWidgets) > 0 else 100
-		self.ui.progressBar.setVisible(True)
+		#progression = 100.0/len(self.listOfWidgets) if len(self.listOfWidgets) > 0 else 100
+		#self.ui.progressBar.setVisible(True)
 		for movie in self.listOfWidgets:
 			QtGui.qApp.processEvents()
-			episode_code = movie.getEpisode()
-			if episode_code == "":
-				movie.setStatus("No Episode","red")
-				continue
-			movie.incrementProgress(10)
-			scene_name = self.getScene(movie.getFilename().upper(),movie.getEpisode())
-			movie.incrementProgress(10)
-			animatic_path = self.getAnimaticPath(episode_code)
-			movie.incrementProgress(10)
-			scene_name += "_" + self.getVersion(scene_name,animatic_path) + ".mov"
-			movie.incrementProgress(10)
-			if not os.path.exists(animatic_path):
-				os.makedirs(animatic_path)
-			movie.incrementProgress(10)
-			dst = os.path.join(animatic_path,scene_name)
-			compressed = os.path.join(temp,movie.getFilename()).replace("\\","/")
-			result = self.compressScene(movie.getFullpath(),compressed) == 0
-			movie.incrementProgress(25)
-			if result:
-				print dst
-				shutil.copyfile(compressed,dst)
-				pass
-			movie.incrementProgress(25)
-			self.incrementProgress(progression)
-			os.remove(compressed)
-			movie.setStatus("Done","green")
-		self.setProgress(100)
+			movie.upload(self.root,self.project_folders,temp)
+			print "Hello world!"
+			#self.incrementProgress(progression)
+
+		#self.setProgress(100)
 		MessageBox.information("Copias feitas com sucesso!")
 
 		return
-
-	def getFFMPEG(self):
-
-		return os.path.join(birdo_app_root,"extra/ffmpeg/windows/bin/ffmpeg.exe").replace("\\","/")
-
-	def compressScene(self,input_scene,output_scene):
-
-	  exe = self.getFFMPEG()
-	  cmd = "{0} -y -i \"{1}\" -vcodec libx264 -pix_fmt yuv420p -g 30 -vprofile high -bf 0 -crf 23 -strict experimental -acodec aac -ab 160k -ac 2 -f mp4 \"{2}\"".format(exe,input_scene,output_scene)
-	  print(cmd)
-	  return os.system(cmd)
-
-	def getVersion(self,scene_name,path):
-
-		print scene_name
-		print [f for f in os.listdir(path) if f.endswith((".mov",".mp4")) and scene_name in f]
-		return "v" + str(len([f for f in os.listdir(path) if f.endswith((".mov",".mp4")) and scene_name in f])+1).zfill(2) if os.path.exists(path) else "v01"
 
 	def getRegexPattern(self,regex,filename):
 
@@ -371,26 +191,6 @@ class Uploader(QtGui.QMainWindow):
 			for i in range(len(index_range)):
 				if m.group(i+1) is not None:
 					return m.group(i+1)
-
-	def getShot(self,filename):
-
-		regex = '.*SC_(\d{4}).*|.*SC(\d{4}).*'
-		index_range = regex.split("|")
-		m = re.search(regex, filename)
-		if m is not None:
-			for i in range(len(index_range)):
-				if m.group(i+1) is not None:
-					return m.group(i+1)
-
-	def getScene(self,filename,episode):
-
-		#m = re.search('(\w{3}_EP\d{3}_SC\d{4}).*', filename)
-		m = self.getShot(filename)
-		return "_".join([self.project_data["prefix"],episode,"SC" + m]) if m is not None else m
-
-	def getAnimaticPath(self,episode_code):
-
-		return os.path.join(self.root,self.project_folders.get_animatic_folder_path(episode_code)).replace("\\","/")
 
 	def getProgress(self):
 		return self.ui.progressBar.value()
@@ -429,14 +229,13 @@ class Uploader(QtGui.QMainWindow):
 			for url in urls:
 				QtGui.qApp.processEvents()
 				u = str(url.toLocalFile())
-				movWidget = VideoItemWidget(u,self.episodes)
-				episode = self.getEpisode(movWidget.getFullpath())
+				movWidget = self.getTemplateItem(u,self.episodes)
+				episode = self.getEpisode(u)
 				if episode is not None:
 					movWidget.setEpisode(self.findIndexOf(episode))
-				if movWidget.isMovie():
+				if movWidget.isValid():
 					self.listOfWidgets.append(movWidget)
 					self.verticalLayout.addWidget(movWidget)
-		                
 		else:
 			e.ignore()
 
