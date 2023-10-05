e134fa8e10c535453bec0290fa103b0f753e1348
Leonardo Bazilio <leobazao@hotmail.com>
2023-10-04 22:07:16 -0700

    update function copyGroupNodeviewCoords in nodeview_utils

--- package/harmony20/BirdoPack/utils/nodeview_utils.js
+++ package/harmony20/BirdoPack/utils/nodeview_utils.js
@@ -12,6 +12,8 @@ function copyGroupsNodeviewCoords(group1, group2){
 	var subs1 = node.subNodes(group1);
 	var n_counter = 0;
 	var w_counter = 0;
+	var w_delete_counter = 0;
+
 	subs1.forEach(function(item, index){
 		var node2 = item.replace(group1, group2);
 		if(node.getName(node2)){
@@ -27,15 +29,35 @@ function copyGroupsNodeviewCoords(group1, group2){
 	Print(Backdrop.setBackdrops(group2, bd1));
 	//update waypoints
 	var wps1 = waypoint.getWaypointsInGroup(group1);
-	wps1.forEach(function(item, index){
+	var wps2 = waypoint.getWaypointsInGroup(group2);
+	wps2.forEach(function(item, index){//remove waypoints
+		var wp1 = item.replace(group2, group1);
+		if(wps1.indexOf(wp1) == -1){
+			waypoint.deleteWaypoint(item);
+			w_delete_counter++;
+		}
+	});
+
+	wps1.forEach(function(item, index){//create waypoints
 		var wp2 = item.replace(group1, group2);
+		if(wps2.indexOf(wp2) == -1){
+			waypoint.add(group2,  item.replace(group1 + "/", ""), 0, 0);
+			var portsChild = waypoint.childInports(item);
+			var nodesChild = waypoint.childNodes(item);
+			var portNode = waypoint.parentNode(item);
+			var portOut = waypoint.parentOutport(item);
+			waypoint.linkOutportToWaypoint(portNode.replace(group1, group2), portOut, wp2);
+			nodesChild.forEach(function(nChild, index){
+				waypoint.linkWaypointToInport(wp2, nChild.replace(group1, group2), portsChild[index], false);
+			});
+		}
 		if(waypoint.setCoord(wp2, waypoint.coordX(item), waypoint.coordY(item))){
 			w_counter++;
 		}
 	});
 	
-	Print("Update Node View Group - Nodes Coords updated: " + n_counter);
-	Print("Update Node View Group - Waypoints Updates: " + w_counter);
-
+	Print("Nodes Coords updated: " + n_counter);
+	Print("Waypoints Updates: " + w_counter);
+	Print("Waypoints deleted: " + w_delete_counter);
 }
 exports.copyGroupsNodeviewCoords = copyGroupsNodeviewCoords;
