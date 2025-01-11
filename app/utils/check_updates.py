033aed1e41199f0c8cf49417766316efcefb3d5d
otmbneto <otmbneto@protonmail.com>
2025-01-10 22:05:06 -0300

    Removed more deprecated functions from check_updates

--- app/utils/check_updates.py
+++ app/utils/check_updates.py
@@ -26,98 +26,6 @@ from app.config_project import config_project
 
 MessageBox = CreateMessageBox()
 
-
-def check_package_update(pack_name):
-    """verifica na rede se ha um update disponivel para o app_package"""
-    app_json = os.path.join(app_root, 'app.json')
-    app_data = read_json_file(app_json)
-    curr_app_version = int(app_data["app_version"].replace(".", ""))
-    new_package_version = int((re.findall(r'\d\.\d\.\d', pack_name)[0]).replace('.', ""))
-    return curr_app_version < new_package_version
-
-
-def get_most_recent_pack(server_packages_list):
-    """pega a lista do folder do birdoAPP no server e retorna o objeto do pack mais recente"""
-    only_zip_packs = filter(lambda x: x.file_type == "file" and x.get_name().endswith(".zip"), server_packages_list)
-    if len(only_zip_packs) == 0:
-        print "nenhum pack encontrado na rede!"
-        return False
-    return sorted(only_zip_packs, key=lambda x: x.get_name())[-1]
-
-
-def install_harmony_package_config(proj_data):
-    """instala o pacote de scripts para o harmony e copia o menu.xml para a pasta correspondente!"""
-    
-    discord_name = proj_data["user_data"]["current_user"]
-    # GAMBS PARA EVITAR PERDER ALTERACEOS NO PACKAGE DO LEO
-    if discord_name == "LeoBazilio#2873":
-        print "gambs Leo para nao perder scripts.."
-        return
-
-    project_prefix = proj_data["prefix"]
-
-    # HARMONY PATHS
-
-    if "harmony_installation" in proj_data["user_data"][discord_name][project_prefix].keys():
-        harmony = ToonBoomHarmony(proj_data["user_data"][discord_name][project_prefix]["harmony_installation"])
-        harmony_script_path = harmony.getScriptsPath()
-        harmony_version = harmony.getVersion()
-    else:
-        harmony_script_path = proj_data["harmony"]["paths"]["scripts"]
-        harmony_version = str(proj_data["harmony"]["version"].split(".")[0])
-
-    # HARMONY BIRDOPACK PATH
-    harmony_script_package_path = os.path.join(harmony_script_path, "packages", "BirdoPack")
-
-    # PACKAGE BIRDOAPP PATH
-    birdo_app_package_root = os.path.join(app_root, 'package', ('harmony' + harmony_version))
-    if not os.path.exists(birdo_app_package_root):
-        print "[ERROR] Package for this harmony version doesn't exist: " + harmony_version
-        return
-        
-    birdo_app_package = os.path.join(birdo_app_package_root, 'BirdoPack')
-    birdo_app_includes = os.path.join(birdo_app_package_root, 'includes')
-
-    # XML CONFIG FILE
-    config_file_pack = os.path.join(app_root, 'config', "Tb{0}_menus.xml".format(harmony_version))
-    config_menu_original_file = proj_data["harmony"]["paths"]["harmony_config_xml"]
-    config_menu_bk_file = config_menu_original_file.replace('menus.xml', 'menus - Copia.xml')
-
-    # COPIA O CONFIG MENUS
-    if os.path.exists(config_menu_bk_file):
-        print "no need to update 'menus.xml' file!"
-    else:
-        print "copying 'menus.xml file..."
-        # CRIA BACKUP MENU FILE
-        copyfile(config_menu_original_file, config_menu_bk_file)
-
-        # COPY menus.xml file
-        copyfile(config_file_pack, config_menu_original_file)
-
-    # IF DON'T HAVE SCRIPT FOLDER YET, CREATE
-    if not os.path.exists(harmony_script_path):
-        os.makedirs(harmony_script_path)
-        print "script folder created: {0}".format(harmony_script_path)
-
-    # UPDATE DO PACKAGE
-    if os.path.exists(harmony_script_package_path):
-        print "removing package before update: {0}".format(harmony_script_package_path)
-        rmtree(harmony_script_package_path) # LIMPA ANTES DE COPIAR
-
-    if not os.path.exists(os.path.dirname(harmony_script_package_path)):
-        print "creating package folder in scripts..."
-        os.makedirs(os.path.dirname(harmony_script_package_path))
-
-    print "updating BirdoPack..."
-    copytree(birdo_app_package, harmony_script_package_path) # COPIA CONTEUDO
-
-    # COPIA INCLUDES
-    for script in os.listdir(birdo_app_includes):
-        script_src_fullpath = os.path.join(birdo_app_includes, script)
-        script_dst_fullpath = os.path.join(harmony_script_path, script)
-        copyfile(script_src_fullpath, script_dst_fullpath)
-        print "include script copied: {0}".format(script)
-
 def install_requirements(main_app=None):
 
     python = sys.executable
@@ -175,16 +83,6 @@ def main_update(proj_data, main_app=None):
     main_app.ui.loading_label.setText("BirdoApp is up-to-date!")
 
     return True
-
-def update_app_version():
-
-    app_file = os.path.join(app_root,"app.json")
-    app_json = read_json_file(app_json)
-    version = app_json["app_version"].replace(".","")
-    version = str(int(version) + 1).zfill(3)
-
-    app_json["app_version"] = ".".join(list(version))
-    write_json_file(app_file,app_json)
 
 if __name__ == "__main__":
     pdata = config_project(app_root, 0)
