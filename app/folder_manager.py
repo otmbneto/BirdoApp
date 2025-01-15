cbfc416fa6b1821a8c9f16954c6b7dfe6272416d
Leonardo Bazilio <leobazao@hotmail.com>
2025-01-14 20:16:49 -0800

    mudei alguns nomes de metodos q estavam confusos, botei o root como parametro em todas funcoes q faziam sentido

--- app/folder_manager.py
+++ app/folder_manager.py
@@ -36,9 +36,9 @@ class FolderManager(object):
             "server": Path(proj_data["paths"]["root"]) / proj_data["paths"]["sub_root"],
             "local": local_folder if bool(local_folder) else ""
         }
-        self.tblib = proj_data["paths"]["tblib"]
-        self.library = proj_data["paths"]["library"]
-        self.episodes = proj_data["paths"]["episodes"]
+        self.tblib = str(proj_data["paths"]["tblib"])
+        self.library = str(proj_data["paths"]["library"])
+        self.episodes = str(proj_data["paths"]["episodes"])
         self.ep = proj_data["paths"]["ep"]
         self.steps = proj_data["paths"]["steps"]
 
@@ -56,9 +56,6 @@ class FolderManager(object):
     def check_connection(self):
         """
         Checa se o servidor esta acessivel.
-        ...
-
-        RETURN: True or False
         """
         try:
             self.server_status = "Online" if self.is_server_available() else "Offline"
@@ -70,142 +67,92 @@ class FolderManager(object):
 
     def get_tblib(self, root):
         """
-        Return tblib folder path in server(no root)
-        ...
-        Parameters
-        ----------
-        root : string
-           local or server
-        ----------
-        RETURN: string
+        Retorna o caminho da tblib com a raiz fornecida (server or local)
         """
         return self.root[root] / self.tblib
 
     def get_episodes_folder(self, root):
         """
         Retorna o caminho com a root (server ou local) da pasta EPISODIOS
-        ...
-        Parameters
-        ----------
-        root : string
-           root (local or server)
-        RETURN: string
         """
         return self.root[root] / self.episodes
 
-    def list_episodes(self):
+    def list_episodes(self, root):
         """
-        Retorna caminhos relativos de todos episodios no server do projeto
-        ...
-        RETURN: list of strings
+        Retorna caminhos de todos episodios na root (server ou local) da pasta EPISODIOS
         """
-        server_ep_folder = self.get_episodes_folder("server")
-        return [x.get_relative_path(self.root["server"]) for x in server_ep_folder.glob(self.regs["ep"])]
+        return self.get_episodes_folder(root).glob(self.regs["ep"])
 
-    def get_scenes_root_folder(self, ep):
+    def get_scenes_root_folder(self, root, ep):
         """
-        Retorna caminho relativo da pasta 'CENAS' do ep no step fornecido.
+        Retorna caminho da pasta 'CENAS' do ep no step fornecido (com root server ou local).
         """
-        return Path(self.episodes) / ep / self.ep["cenas"]["folder"]
+        return self.get_episodes_folder(root) / ep / self.ep["cenas"]["folder"]
 
-    def get_episode_scenes_path(self, ep, step):
+    def get_scenes_path(self, root, ep, step):
         """
-        Retorna caminho relativo da pasta 'CENAS' do ep no step fornecido.
+        Retorna caminho das cenas do ep e step fornecido. Com root server - local.
         """
         if step not in self.steps:
             raise Exception("Step '{0}' not in self.steps parameters.".format(step))
-        return self.get_scenes_root_folder(ep) / self.steps[step]["folder_name"]
+        return self.get_scenes_root_folder(root, ep) / self.steps[step]["folder_name"]
 
-    def get_scenes(self, ep, step):
-        """retorna lista de cenas do ep e step"""
-        server_scenes = self.root["server"] / self.get_episode_scenes_path(ep, step)
-        return server_scenes.glob(self.regs["scene"])
+    def list_scenes(self, root, ep, step):
+        """retorna lista de cenas no servidor do ep e step"""
+        return self.get_scenes_path(root, ep, step).glob(self.regs["scene"])
 
-    def get_scene_path(self, scene_name, step):
-        """retorna caminho da cena (sem root)
+    def get_scene_path(self, root, scene_name, step):
+        """retorna caminho completo da cena.
         """
-        if not bool(re.match(self.regs["scene"], scene_name)):
+        scene = re.findall(self.regs["scene"], scene_name)
+        if len(scene) == 0:
             raise Exception("Invalid scene name: {0}".format(scene_name))
+        scene_name = scene[0]
+        ep = re.compile(self.regs["ep"]).findall(scene_name)[0]
+        return self.get_scenes_path(root, ep, step) / scene_name
 
-        ep = re.findall(self.regs["scene"], scene_name)[0]
-        return self.get_episode_scenes_path(ep, step) / scene_name
+    def get_renders_root(self, root, ep):
+        """
+            Retorna folder base de renders do ep.
+        """
+        return self.get_scenes_root_folder(root, ep) / self.ep["cenas"]["render"]["folder"]
 
-    def get_renders_root(self, ep):
+    def get_animatics_folder(self, root, ep):
         """
-            Retorna folder relativo de render do ep
+        Retorna folder de animatics do ep.
         """
-        return self.get_scenes_root_folder(ep) / self.ep["cenas"]["render"]["folder"]
+        return self.get_renders_root(root, ep) / self.ep["cenas"]["render"]["sub_folders"][0]
 
-    def get_animatics_folder(self, ep):
+    def get_render_folder(self, root, ep, step):
         """
-        Retorna folder relativo do animatic do ep
+        Retorna caminho do render de step. (com root server ou local)
         """
-        return self.get_renders_root(ep) / self.ep["cenas"]["render"]["sub_folders"][0]
+        return self.get_renders_root(root, ep) / self.steps[step]["folder_name"]
 
-    def get_render_folder(self, ep, step):
-        """
-        Retorna caminho relativo do render de step fornecido
-        """
-        return self.get_renders_root(ep) / self.steps[step]["folder_name"]
+    def list_project_animatics(self, ep):
+        """Retorna lista de movs animatics do ep."""
+        return self.get_animatics_folder("server", ep).glob(self.regs["animatic"])
 
-    # def create_local_scene_scheme(self, scene_name, step):
-    #     """
-    #     Create local folder scheme for the scene in the selected step and return scene full path
-    #     (folder list in self.step[step]["local"])
-    #     ...
-    #
-    #     Parameters
-    #     ----------
-    #     scene_name : string
-    #        scene name (PRJ_EPXXX_SCXXXX)
-    #     step : string
-    #         step name (must be in self.step)
-    #     ----------
-    #     RETURN: string
-    #     """
-    #     root = self.get_local_root()
-    #     scene_path = root + self.get_scene_path(scene_name, step)
-    #     sub_folders = self.step[step]["local"]
-    #     for folder in sub_folders:
-    #         sub_folder = os.path.join(scene_path, folder)
-    #         if os.path.exists(sub_folder):
-    #             print 'folder already exists {0}'.format(sub_folder)
-    #             continue
-    #         try:
-    #             os.makedirs(sub_folder)
-    #             print "folder created: {0}".format(sub_folder)
-    #         except:
-    #             self.mb.warning("ERRO ao criar o folder: {0}".format(sub_folder))
-    #             return False
-    #     return scene_path
-    #
-    # def create_local_render_scheme(self, ep):
-    #     """
-    #     Create local folder scheme for the episode render folder and return it full path
-    #     (folder list in self.step["RENDER"])
-    #     ...
-    #
-    #     Parameters
-    #     ----------
-    #     scene_name : string
-    #        scene name (PRJ_EPXXX_SCXXXX)
-    #     step : string
-    #         step name (must be in self.step)
-    #     ----------
-    #     RETURN: string
-    #     """
-    #     root = self.get_local_root()
-    #     render_folder = os.path.join(root, self.get_render_path(ep))
-    #     render_subs = self.step["RENDER"]
-    #     for folder in render_subs:
-    #         sub_folder = os.path.join(render_folder, folder)
-    #         if os.path.exists(sub_folder):
-    #             print 'folder already exists {0}'.format(sub_folder)
-    #             continue
-    #         try:
-    #             os.makedirs(sub_folder)
-    #             print "folder created: {0}".format(sub_folder)
-    #         except:
-    #             self.mb.warning("ERRO ao criar o folder: {0}".format(sub_folder))
-    #             return False
-    #     return render_folder
+    def create_scene_scheme(self, root, scene_name, step):
+        """
+            Cria o esquema de cenas do root fornecido (server ou local)
+            (retorna o caminho da cena)
+        """
+        scene = self.get_scene_path(root, scene_name, step)
+        for item in self.steps[step][root]:
+            f = scene / item
+            f.make_dirs()
+        return scene
+
+    def create_project_server_folders(self):
+
+
+if __name__ == "__main__":
+    from utils.MessageBox import CreateMessageBox
+    from utils.birdo_json import read_json_file
+    MessageBox = CreateMessageBox()
+    pdata = read_json_file(r"C:\_BirdoRemoto\_birdo2_testes\_fake_server\_config_projects_teste\LEB\project_data.json")
+    lcaolf = Path("C:/_BirdoRemoto/_birdo2_testes/_fake_local/LUPI")
+    fm = FolderManager(pdata, lcaolf, MessageBox)
+    print fm.list_episodes()
+    print "END"
