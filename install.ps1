f74361b214bc855a7ab5f7f6d422699fd457073b
unknown <Administrator@birdo.local>
2025-01-21 02:52:27 -0300

    Remocao de chamar o script em permissao de admin.
    uma vez que nao e mais necessaria escalar privilegios (nao mais
    altera pastas do sistema e cria variaveis de ambiente p/ usuario)
    removido o trecho inicial que executava o script em uma nova sessao.

--- install.ps1
+++ install.ps1
@@ -1,7 +1,3 @@
-if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
-    Exit
-}
 
 
 #TODO : Checar se python install existe - Done
