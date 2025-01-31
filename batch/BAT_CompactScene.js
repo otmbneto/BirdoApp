4dcc7acd7cec918b3293a083f8435976216db9a9
Leonardo Bazilio <leobazao@hotmail.com>
2025-01-30 18:51:37 -0800

    acetei o script batch de compact scene para deletar os arquivos nao usados

--- batch/BAT_CompactScene.js
+++ batch/BAT_CompactScene.js
@@ -1,7 +1,21 @@
 include("BD_1-ScriptLIB_File.js");
 include("BD_2-ScriptLIB_Geral.js");
+/* 
+-------------------------------------------------------------------------------
+Name:		BAT_CompactScene.js
 
+Description: BATCH script para limpar a cena e deletar os arquivos nao usados da versao mais recente.
 
+Usage:		USAR COMO BATCH (ATENCAO: somente use esse script em uma cena que tenha CERTEZA que quer deletar os arquivos nao usados da cena.
+
+Author:		Leonardo Bazilio Bentolila
+
+Created:	setembro, 2025
+            
+Copyright:   leobazao_@Birdo
+ 
+-------------------------------------------------------------------------------
+*/
 //runs batch
 BAT_CompactScene();
 
@@ -21,9 +35,19 @@ function BAT_CompactScene(){
 		var allfiles = BD1_ListFolderRecursivelly(scene.currentProjectPath())
 		var used_nodes_list = nodes_data["file_list"].map(function(item){ return item["full_path"]});
 		var delete_list = allfiles.filter(function(item){ return used_nodes_list.indexOf(item) == -1});
-		Print(delete_list);
-		Print("delete list: " + delete_list.length);
-		Print("used list: " + used_nodes_list.length);
+
+		Print("deletando " + delete_list.length + " arquivos...");
+		var counter = 0;
+		delete_list.forEach(function(item){
+			if(BD1_is_file(item)){
+				BD1_RemoveFile(item);
+				counter++;
+			} else {
+				BD1_RemoveDirs(item);
+				counter++;
+			}
+		});
+		
 	}catch(e){
 		Print(e);
 	}
