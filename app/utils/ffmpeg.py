0d423ab65c96050b0c3ca15568df1ca8bda47579
Leonardo Bazilio <leobazao@hotmail.com>
2025-02-10 09:50:06 -0800

    nova classe com opcoes avancadas do ffmepg (com novos metodos e todos com opcao de salvar log)

--- app/utils/ffmpeg.py
+++ app/utils/ffmpeg.py
@@ -1,6 +1,13 @@
+"""
+    Funcoes simples para rodar acoes do ffmpeg
+    (as acoes de processamento de arquivos gera logs no temp)
+"""
 import os
+import subprocess
+import re
 from system import SystemFolders
-from time import time
+from datetime import datetime
+import time
 curr_dir = os.path.dirname(os.path.realpath(__file__))
 birdo_app_root = os.path.dirname(os.path.dirname(curr_dir))
 syst = SystemFolders()
@@ -14,8 +21,57 @@ def compress_render(input_file, output_file):
     """Compressao basica (retirada do shotgun) do render para upload"""
     vcodec = "-vcodec libx264 -pix_fmt yuv420p -g 30 -vprofile high -bf 0 -crf 23"
     acodec = "-strict experimental -acodec aac -ab 160k -ac 2"
-    log_file = ffmpeg_log / "{0}_{1}.log".format(int(time()), os.path.basename(input_file))
+    log_file = ffmpeg_log / "{0}_{1}.log".format(int(time.time()), os.path.basename(input_file))
     cmd = "{0} -i {1} {2} {3} -f mp4 {4} 2> {5}".format(
         ffmpeg_path, input_file, vcodec, acodec, output_file, log_file.path
     )
     return os.system(cmd) == 0
+
+
+def convert_movie_to_image_seq(input_mov, output_folder, img_format, scale_size=None):
+    """converte um arquivo de video em uma sequecia de imagem no destino 'output_folder'"""
+    img_out = "{0}/f-%04d.{1}".format(output_folder, img_format)
+    scale = "scale=iw/{0}:ih/{0} ".format(scale_size) if scale_size is not None else " "
+    log_file = ffmpeg_log / "{0}_{1}.log".format(int(time.time()), os.path.basename(input_mov))
+    cmd = "{0} -i {1} -vf {2}{3} 2> {4}".format(
+        ffmpeg_path, input_mov, scale, img_out, log_file.path
+    )
+    return os.system(cmd) == 0
+
+
+def extract_audio(input_mov_file, output_audio_file):
+    """converte o arquivo de video em um arquivo de audio"""
+    log_file = ffmpeg_log / "{0}_{1}.log".format(int(time.time()), os.path.basename(input_mov_file))
+    cmd = "{0} -i {1} {2} 2> {3}".format(
+        ffmpeg_path, input_mov_file, output_audio_file, log_file.path
+    )
+    return cmd
+    # return os.system(cmd) == 0
+
+
+def get_video_frames_duration(video_file):
+    """retorna a duracao do video em frames"""
+    try:
+        subprocess.check_output("{0} -i {1}".format(ffmpeg_path, video_file), stderr=subprocess.STDOUT, shell=True)
+    except subprocess.CalledProcessError as exc:
+        fps = re.findall(r"\d+\sfps", exc.output)
+        duration = re.findall(r"Duration:\s\d{2}:\d{2}:\d+\.?\d+", exc.output)
+        if len(duration) == 0 or len(fps) == 0:
+            print "[BIRDOAPP] - nao foi possivel encontrar a duracao do arquivo: {0}".format(video_file)
+            return None
+        t = datetime.strptime(duration[0].replace("Duration: ", ""), "%H:%M:%S.%f")
+        fps_int = int(re.findall(r"\d+", fps[0])[0])
+        return int(round(fps_int * (t.second + (t.microsecond * 1e-6))))
+
+
+def check_audio_stream(video_file):
+    """checa se o arquivo de video tem faixa de audio"""
+    try:
+        subprocess.check_output("{0} -i {1}".format(ffmpeg_path, video_file), stderr=subprocess.STDOUT, shell=True)
+    except subprocess.CalledProcessError as exc:
+        return bool(re.findall(r"Stream\s.+\sAudio", exc.output))
+
+
+if __name__ == "__main__":
+    inmov = r"C:\_BirdoRemoto\PROJETOS\LupiBaduki\LEB_EP101_SC0020.mov"
+    print extract_audio(inmov, r"C:\_BirdoRemoto\PROJETOS\LupiBaduki\LEB_EP101_SC0020_TESTE.wav")
---
+++ app/utils/ffmpeg_advanced.py
@@ -0,0 +1,116 @@
+"""
+    Classe com metodos avancados para processar acoes do ffmpeg
+    usando tqdm progress e salvando logs das acoes no temp
+"""
+import subprocess
+import shlex
+import re
+import os
+from datetime import datetime
+from tqdm import tqdm
+
+
+class CoverterFFMPEG:
+    """Main ffmpeg exporter class."""
+    def __init__(self, temp_folder, ffmpeg_exe="ffmpeg"):
+        self.ffmpeg = ffmpeg_exe
+        self.temp_folder = temp_folder
+
+        # ffmpeg command
+        self.cmd = None
+
+        # frame regex to find  frame num in stderr lines
+        self.frame_reg = re.compile(r'frame=?\s+\d+')
+
+        # progress bar
+        self.pb = None
+
+        # curdir original (usado para gambs de salvar os logs no temp)
+        self.initial_dir = os.curdir
+
+    def update_render_progress(self, sterr_lin):
+        """update the render progress line stdout."""
+        # check for render frame value
+        matches = self.frame_reg.findall(str(sterr_lin))
+        if len(matches) != 0:
+            frame = int(re.findall(r'\d+', matches[0])[0])
+            if self.pb is not None:
+                self.pb.update(frame - self.pb.n)
+                self.pb.set_description_str('[BIRDOAPP - ffmpeg] frame: {0}'.format(frame))
+            else:
+                print "[BIRDOAPP - ffmpeg] - frame {0}".format(frame)
+
+    def run_command(self):
+        """run convert images to movie command."""
+        process = subprocess.Popen(shlex.split(self.cmd),
+                                   shell=True,
+                                   stderr=subprocess.STDOUT,
+                                   stdout=subprocess.PIPE,
+                                   cwd=self.temp_folder,
+                                   universal_newlines=True)
+
+        for line in process.stdout:
+            self.update_render_progress(str(line))
+            process.stdout.flush()
+
+        # closes progress bar
+        self.pb.close()
+        self.pb = None
+        os.chdir(self.initial_dir)
+        print "[BIRDOAPP] comando ffmpeg finalizado!"
+        return process.returncode is None
+
+    def get_video_duration(self, video_file):
+        """retorna a duracao do video em frames"""
+        try:
+            subprocess.check_output("{0} -i {1}".format(self.ffmpeg, video_file), stderr=subprocess.STDOUT, shell=True)
+        except subprocess.CalledProcessError as exc:
+            fps = re.findall(r"\d+\.?\d+\sfps", exc.output)
+            duration = re.findall(r"Duration:\s\d{2}:\d{2}:\d+\.?\d+", exc.output)
+            if len(duration) == 0 or len(fps) == 0:
+                print "[BIRDOAPP] - nao foi possivel encontrar a duracao do arquivo: {0}".format(video_file)
+                return None
+            t = datetime.strptime(duration[0].replace("Duration: ", ""), "%H:%M:%S.%f")
+            fps_int = float(re.findall(r"\d+\.?\d+", fps[0])[0])
+            return int(round(fps_int * (t.second + (t.microsecond * 1e-6))))
+
+    def check_audio_stream(self, video_file):
+        """checa se o arquivo de video tem faixa de audio"""
+        try:
+            subprocess.check_output("{0} -i {1}".format(self.ffmpeg, video_file), stderr=subprocess.STDOUT, shell=True)
+        except subprocess.CalledProcessError as exc:
+            return bool(re.findall(r"Stream\s.+\sAudio", exc.output))
+
+    def compress_video(self, input_file, output_file):
+        """Compressao basica (retirada do shotgun) do render para upload"""
+        vcodec = "-vcodec libx264 -pix_fmt yuv420p -g 30 -vprofile high -bf 0 -crf 23"
+        acodec = "-strict experimental -acodec aac -ab 160k -ac 2 " if self.check_audio_stream(input_file) else ""
+        self.cmd = "{0} -report -i {1} {2} {3}-f mp4 {4}".format(
+            self.ffmpeg, input_file, vcodec, acodec, output_file
+        )
+        total_frames = self.get_video_duration(input_file)
+        if total_frames:
+            self.pb = tqdm(total=total_frames, leave=True, desc="[BIRDOAPP - ffmpeg] Compress file ")
+        return self.run_command()
+
+    def convert_movie_to_image_seq(self, input_mov, output_folder, img_format, scale_size=None):
+        """converte um arquivo de video em uma sequecia de imagem no destino 'output_folder'"""
+        img_out = "{0}/f-%04d.{1}".format(output_folder, img_format)
+        scale = "scale=iw/{0}:ih/{0} ".format(scale_size) if scale_size is not None else " "
+        self.cmd = "{0} -report -i {1} -vf {2}{3}".format(
+            self.ffmpeg, input_mov, scale, img_out
+        )
+        total_frames = self.get_video_duration(input_mov)
+        if total_frames:
+            self.pb = tqdm(total=total_frames, leave=True, desc="[BIRDOAPP - ffmpeg] Get Image Sequence ")
+        return self.run_command()
+
+    def extract_audio(self, input_mov_file, output_audio_file):
+        """converte o arquivo de video em um arquivo de audio"""
+        self.cmd = "{0} -report -i {1} {2}".format(
+            self.ffmpeg, input_mov_file, output_audio_file
+        )
+        os.chdir(self.temp_folder)
+        r = os.system(self.cmd)
+        os.chdir(self.initial_dir)
+        return r == 0
