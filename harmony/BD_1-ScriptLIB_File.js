26a853482e1d9afa11a3e1d1c5f8ad96aeee4703
Leonardo Bazilio <leobazao@hotmail.com>
2025-05-05 08:28:41 -0700

    ajustei as funcoes de ffmpeg para rodar com o script do python, e acertei formatacao do arquivo

--- harmony/BD_1-ScriptLIB_File.js
+++ harmony/BD_1-ScriptLIB_File.js
@@ -17,216 +17,216 @@ Copyright:  leobazao_@Birdo
 */
 
 //#####################JSON files#####################//
-	/*cria arquivo JSON com objeto e caminho dados como parametro
-	@objeto => javascript object
-	@fileName => arquivo json a ser criado
-	*/
+/*cria arquivo JSON com objeto e caminho dados como parametro
+@objeto => javascript object
+@fileName => arquivo json a ser criado
+*/
 
-	function BD1_WriteJsonFile(objeto, fileName){
-		var jsonString = JSON.stringify(objeto, null, 2);
-		var file = new File(fileName);
-		try {
-			file.open(FileAccess.WriteOnly);
-			file.write(jsonString);
-			file.close();
-			MessageLog.trace("JSON File created! " + fileName);
-			return true;
-		} catch (err){
-			Print(err);
-		}
+function BD1_WriteJsonFile(objeto, fileName){
+	var jsonString = JSON.stringify(objeto, null, 2);
+	var file = new File(fileName);
+	try {
+		file.open(FileAccess.WriteOnly);
+		file.write(jsonString);
+		file.close();
+		MessageLog.trace("JSON File created! " + fileName);
+		return true;
+	} catch (err){
+		Print(err);
+	}
+	return false;
+}
+
+/*le o arquivo JSON e retorna o objeto javascript
+@jsonFile => arquivo json a ser covertido
+*/
+function BD1_ReadJSONFile(json_path){
+	var file = new File(json_path);
+	if(!file.exists){
+		MessageLog.trace("Convert JSON to Object ERRO: Arquivo dado como parametro nao existe!");
+		return false;
+	}	
+	file.open(FileAccess.ReadOnly);
+	var json_object = file.read(json_path);
+	file.close();
+	return JSON.parse(json_object);
+}
+
+//##################### FOLDERS #####################//
+/*Teste se o folder existe
+@dirPath => folder a ser testado
+*/
+function BD1_DirExist(dirPath){
+	var dir = new Dir(dirPath);
+	return dir.exists;
+}
+
+/*Remove a pasta dada como parametro (recursivo)
+@dirPath => folder a ser deletado
+*/
+function BD1_RemoveDirs(dirPath){
+	var dir = new Dir;
+	dir.path = dirPath;
+	if(!dir.exists){
+		MessageLog.trace("[REMOVEDIR] Diretorio nao encontrado: " + dirPath);
 		return false;
 	}
-
-	/*le o arquivo JSON e retorna o objeto javascript
-	@jsonFile => arquivo json a ser covertido
-	*/
-	function BD1_ReadJSONFile(json_path){
-		var file = new File(json_path);
-		if(!file.exists){
-			MessageLog.trace("Convert JSON to Object ERRO: Arquivo dado como parametro nao existe!");
-			return false;
-		}	
-		file.open(FileAccess.ReadOnly);
-		var json_object = file.read(json_path);
-		file.close();
-		return JSON.parse(json_object);
+	try {
+		dir.rmdirs();
+		MessageLog.trace("[REMOVEDIR] Diretorio removido..." + dirPath);
+	} catch (err){
+		Print(err);
+		return false;
 	}
-	
-//##################### FOLDERS #####################//
-	/*Teste se o folder existe
-	@dirPath => folder a ser testado
-	*/
-	function BD1_DirExist(dirPath){
-		var dir = new Dir(dirPath);
-		return dir.exists;
-	}
+	return true;
+}
 
-	/*Remove a pasta dada como parametro (recursivo)
-	@dirPath => folder a ser deletado
-	*/
-	function BD1_RemoveDirs(dirPath){
-		var dir = new Dir;
-		dir.path = dirPath;
-		if(!dir.exists){
-			MessageLog.trace("[REMOVEDIR] Diretorio nao encontrado: " + dirPath);
-			return false;
-		}
+/*cria diretorio recusivamente 
+@dirPath => folder a ser criado
+*/
+function BD1_makeDir(dirPath){
+	var myDir = new Dir;
+	myDir.path = dirPath;
+	if(!myDir.exists){
 		try {
-			dir.rmdirs();
-			MessageLog.trace("[REMOVEDIR] Diretorio removido..." + dirPath);
-		} catch (err){
-			Print(err);
+			myDir.mkdirs(dirPath);
+		} catch (e){
+			Print(e);
 			return false;
 		}
 		return true;
 	}
+	return false;
+}
 
-	/*cria diretorio recusivamente 
-	@dirPath => folder a ser criado
-	*/
-	function BD1_makeDir(dirPath){
-		var myDir = new Dir;
-		myDir.path = dirPath;
-		if(!myDir.exists){
-			try {
-				myDir.mkdirs(dirPath);
-			} catch (e){
-				Print(e);
+/*cria diretorios recusvivamente (feito pra funcionar na rede)
+@path => folder a ser criado
+*/
+function BD1_createDirectoryREDE(path){
+
+	if(path.length == 0){
+		return false;
+	}
+
+	var save_dir = null;
+	var p = path.split("/");
+	var sub_path = p[0] + "/";
+
+	//começa em 1 pra evitar tentar criar a raiz do vpn
+	for(var i = 1; i < p.length; i++){
+		sub_path += p[i] + "/";
+		save_dir = new Dir(sub_path);
+		if(!save_dir.exists){
+
+			try{
+				save_dir.mkdir();
+				MessageLog.trace("directory created: " + sub_path);
+			}
+			catch(error){
+				MessageLog.trace( error);
 				return false;
 			}
-			return true;
 		}
+	}
+	return true;
+} 
+/*Lista o folder dado ignorando os . .. no output
+@main_folder => folder a ser analizado para listar folders
+*/
+function BD1_ListFolders(main_folder){
+	var dir = new Dir;
+	dir.path = main_folder;
+	var folderList = dir.entryList("*", 1, 4);
+	folderList.shift();
+	folderList.shift();
+	return folderList;
+}
+
+/*lista todos itens dentro do folder recursivamente, e retorna lista de fullPaths dos arquivos/dirs
+@rootFolder => caminho do folder para listar conteudo
+*/
+function BD1_ListFolderRecursivelly(rootFolder){
+	var finalList = [];
+	
+	listRecursively(rootFolder);
+
+	function listRecursively(mainFolder){
+		var dir = new Dir;
+		dir.path = mainFolder;
+		var contentList = dir.entryList("*", 3, 4).filter(function(x){ return x != "." && x != ".."});
+		for(var i=0; i<contentList.length; i++){
+			var fullPath = dir.filePath(contentList[i]);
+			finalList.push(fullPath);
+			if(!BD1_is_file(fullPath)){
+				listRecursively(fullPath);
+			}
+		}
+	}
+	return finalList;
+}
+
+/*se for mac, pega o caminho e bota aspas nele ""
+@path => caminho a ser analizado
+*/	
+function BD1_addScapeForMac(path){
+	if(about.isWindowsArch()){
+		var wScape = "\"\"" + path + "\"\"";
+		return wScape;
+	}
+	return path;
+}
+
+/*se for Windows troca as "/" por "\\"
+@path => caminho pra trocar
+DEPRECIATED => usar normalize function instead!!!!
+*/	
+function BD1_doubleBackSlash_forWindows(path){
+	if(about.isWindowsArch()){
+		path = path.replace(/\//g, "\\\\");
+		return path;
+	}
+	return path;
+}	
+/*se for Windows troca as "/" por "\\"
+	@path => caminho pra trocar
+	@os_force => bool pra determinar se forca o OS (se for windows usa o \\ pra separar)
+*/	
+function BD1_normalize_path(path, os_force){
+	var os_bar = os_force && about.isWindowsArch() ? "\\" : "/";
+	var regex_bars = /\/|\\/;//gets both fowardslash bars and backslash
+	return path.split(regex_bars).join(os_bar);
+}
+
+/*
+	abre o caminho no explorer
+*/
+function BD1_OpenFolder(path){
+	if(!BD1_DirExist(path)){
+		Print("Open folder error! folder nao existe : " + path);
+		return false;
+	} 
+	var process = null;
+	var folderPath = fileMapper.toNativePath(path);
+	if(about.isWindowsArch()){
+		var command = System.getenv("windir") + "/explorer.exe";
+		process = new Process2(fileMapper.toNativePath(command), folderPath);
+	} else if(about.isMacArch()){
+		process = new Process2("open", folderPath);
+	}
+
+	if(!process){
+		MessageBox.warning("Open folder not suported for this OS!", 0,0);
 		return false;
 	}
-
-	/*cria diretorios recusvivamente (feito pra funcionar na rede)
-	@path => folder a ser criado
-	*/
-	function BD1_createDirectoryREDE(path){
-
-		if(path.length == 0){
-			return false;
-		}
-	
-		var save_dir = null;
-		var p = path.split("/");
-		var sub_path = p[0] + "/";
-
-		//começa em 1 pra evitar tentar criar a raiz do vpn
-		for(var i = 1; i < p.length; i++){
-			sub_path += p[i] + "/";
-			save_dir = new Dir(sub_path);
-			if(!save_dir.exists){
-
-				try{
-					save_dir.mkdir();
-					MessageLog.trace("directory created: " + sub_path);
-				}
-				catch(error){
-					MessageLog.trace( error);
-					return false;
-				}
-			}
-		}
-		return true;
-	} 
-	/*Lista o folder dado ignorando os . .. no output
-	@main_folder => folder a ser analizado para listar folders
-	*/
-	function BD1_ListFolders(main_folder){
-		var dir = new Dir;
-		dir.path = main_folder;
-		var folderList = dir.entryList("*", 1, 4);
-		folderList.shift();
-		folderList.shift();
-		return folderList;
+	var ret = process.launch();// let's home this worked.
+	Print(ret);
+	if(ret != 1){
+		MessageBox.information("Erro ao tentar abrir folder: " + folderPath);
+		return false;
 	}
-	
-	/*lista todos itens dentro do folder recursivamente, e retorna lista de fullPaths dos arquivos/dirs
-	@rootFolder => caminho do folder para listar conteudo
-	*/
-	function BD1_ListFolderRecursivelly(rootFolder){
-		var finalList = [];
-		
-		listRecursively(rootFolder);
-
-		function listRecursively(mainFolder){
-			var dir = new Dir;
-			dir.path = mainFolder;
-			var contentList = dir.entryList("*", 3, 4).filter(function(x){ return x != "." && x != ".."});
-			for(var i=0; i<contentList.length; i++){
-				var fullPath = dir.filePath(contentList[i]);
-				finalList.push(fullPath);
-				if(!BD1_is_file(fullPath)){
-					listRecursively(fullPath);
-				}
-			}
-		}
-		return finalList;
-	}
-	
-	/*se for mac, pega o caminho e bota aspas nele ""
-	@path => caminho a ser analizado
-	*/	
-	function BD1_addScapeForMac(path){
-		if(about.isWindowsArch()){
-			var wScape = "\"\"" + path + "\"\"";
-			return wScape;
-		}
-		return path;
-	}
-
-	/*se for Windows troca as "/" por "\\"
-	@path => caminho pra trocar
-	DEPRECIATED => usar normalize function instead!!!!
-	*/	
-	function BD1_doubleBackSlash_forWindows(path){
-		if(about.isWindowsArch()){
-			path = path.replace(/\//g, "\\\\");
-			return path;
-		}
-		return path;
-	}	
-	/*se for Windows troca as "/" por "\\"
-		@path => caminho pra trocar
-		@os_force => bool pra determinar se forca o OS (se for windows usa o \\ pra separar)
-	*/	
-	function BD1_normalize_path(path, os_force){
-		var os_bar = os_force && about.isWindowsArch() ? "\\" : "/";
-		var regex_bars = /\/|\\/;//gets both fowardslash bars and backslash
-		return path.split(regex_bars).join(os_bar);
-	}
-
-	/*
-		abre o caminho no explorer
-	*/
-	function BD1_OpenFolder(path){
-		if(!BD1_DirExist(path)){
-			Print("Open folder error! folder nao existe : " + path);
-			return false;
-		} 
-		var process = null;
-		var folderPath = fileMapper.toNativePath(path);
-		if(about.isWindowsArch()){
-			var command = System.getenv("windir") + "/explorer.exe";
-			process = new Process2(fileMapper.toNativePath(command), folderPath);
-		} else if(about.isMacArch()){
-			process = new Process2("open", folderPath);
-		}
-
-		if(!process){
-			MessageBox.warning("Open folder not suported for this OS!", 0,0);
-			return false;
-		}
-		var ret = process.launch();// let's home this worked.
-		Print(ret);
-		if(ret != 1){
-			MessageBox.information("Erro ao tentar abrir folder: " + folderPath);
-			return false;
-		}
-		Print("Folder opened: " + folderPath);
-		return true;
-	}
+	Print("Folder opened: " + folderPath);
+	return true;
+}
 
 //#####################GENERAL files#####################//
 function BD1_fileBasename(filePath){//retorna o nome do arquivo com extensao
@@ -489,31 +489,22 @@ function BD1_Get_ffmpeg(birdoAPP_root){
 
 /* 
 converte o arquivo de video dado para uma seq de imagens no output folder tmb dado
-@movie - arquivo de video a ser convertido
+@input_file - arquivo de video a ser convertido
 @outPutFolder - folder de saida das imagens
 */
-function BD1_convert_mov_to_images(birdoAPP_root, movie, outPutFolder, extension){
-	var ffmpeg = BD1_Get_ffmpeg(birdoAPP_root);
-	var images = outPutFolder + "f-%04d." + extension;
-	System.processOneEvent();
-
-	//get reduced size of image based on scene size
-	var scene_x = scene.defaultResolutionX();
-	var scene_y = scene.defaultResolutionY();
-	var y = 270;
-	var x = Math.round(scene_x/(scene_y/y));
-	var res = [x, y].join("x");
-
-	var process = new Process2(ffmpeg, "-i", BD2_FormatPathOS(movie), "-r", 24, "-s", res, images);
-	var ret = process.launch();// let's home this worked.
+function BD1_convert_mov_to_images(birdoAPP_root, input_file, outPutFolder, extension){
+	var pythonPath = BD2_FormatPathOS(birdoAppRoot + "venv/Scripts/python");
+	var pyFile = BD2_FormatPathOS(birdoAppRoot + "app/utils/harmony_ffmpeg.py");
+	var start = Process2(pythonPath, pyFile, "mov2img", "-i", input_file, "-f", extension, "-o", outPutFolder);
+	var ret = start.launch();
 
 	if(ret != 0){
-		MessageLog.trace("Erro ao converter movie: " + BD2_FormatPathOS(movie));
-		return false;
-	} else {
-		MessageLog.trace("Movie : " + movie + " converted to image sequence in : " + outPutFolder);
-		return true;
+		Print("[BIRDOAPP] falha ao rodar o script de ffmpeg no python!");
+		return false; 
 	}
+
+	Print("[BIRDOAPP] imagens geradas no folder: " + outPutFolder);
+	return true;
 }
 
 /*
@@ -523,20 +514,21 @@ Compressao basica usando ffmpeg (retirada do shotgun) do render para upload
 @output_file - saida do arquivo
 */
 function BD1_CompressMovieFile(birdoAppRoot, input_file, output_file){
-	var ffmpeg = BD1_Get_ffmpeg(birdoAppRoot);
-Print("TESTE FFMPEG: " + ffmpeg);
-	var vcodec = "-vcodec libx264 -pix_fmt yuv420p -g 30 -vprofile high -bf 0 -crf 23"
-	var acodec = "-strict experimental -acodec aac -ab 160k -ac 2"
-	var start = Process2(ffmpeg, "-i", input_file, vcodec, acodec, "-f", "mp4", BD2_FormatPathOS(output_file));
+	var pythonPath = BD2_FormatPathOS(birdoAppRoot + "venv/Scripts/python");
+	var pyFile = BD2_FormatPathOS(birdoAppRoot + "app/utils/harmony_ffmpeg.py");
+	var start = Process2(pythonPath, pyFile, "compress", "-i", input_file, "-o", output_file);
 	var ret = start.launch();
-	
-	if(ret == 0){
-		Print("Movie converted with ffmepg: " + output_file);
-		return true;
-	} else {
-		Print("[FFMPEG COMPRESS ERROR] Errror compressing movie file: " + input_file);
-		return false;
+
+	if(ret != 0){
+		Print("[BIRDOAPP] falha ao rodar o script de ffmpeg no python!");
+		return false; 
 	}
+	if(BD1_FileExists(output_file)){
+		Print("[BIRDOAPP] movie comprimido: " + output_file);
+		return true;
+	}
+	Print("[BIRDOAPP] erro ao gerar o movie: " + output_file);
+	return false;
 }
 
 /*
@@ -549,19 +541,21 @@ comprime uma seq de imagens em um mov
 @input_file - arquivo para ser convertido
 @output_file - saida do arquivo
 */
-function BD1_MakeMovieFromImageSeq(birdoAppRoot, startFrame, imagePatern, duration, fps, output_file){
-	var ffmpeg = BD1_Get_ffmpeg(birdoAppRoot);
-	var vcodec = "-c:v libx264 -pix_fmt yuv420p -g 30 -vprofile high -bf 0 -crf  3";
-		var start = Process2(ffmpeg, "-y", "-start_number", startFrame, "-i", imagePatern, "-vframes", duration, "-r", fps, vcodec, BD2_FormatPathOS(output_file));
-	var ret = start.launch();
-		
-	if(ret == 0){
-		Print("Movie converted with ffmepg: " + output_file);
-		return true;
-	} else {
-		Print("[FFMPEG COMPRESS ERROR] Errror compressing movie file: " + output_file);
-		return false;
+function BD1_MakeMovieFromImageSeq(birdoAppRoot, imagePatern, fps, audio_file, output_file){
+	var pythonPath = BD2_FormatPathOS(birdoAppRoot + "venv/Scripts/python");
+	var pyFile = BD2_FormatPathOS(birdoAppRoot + "app/utils/harmony_ffmpeg.py");
+	if(!BD1_FileExists(audio_file)){
+		audio_file = "null";
 	}
+	var start = Process2(pythonPath, pyFile, "img2mov", "-i", imagePatern, "-fps", fps, "-a", audio_file, "-o", output_file);
+	var ret = start.launch();
+
+	if(ret != 0){
+		Print("[BIRDOAPP] falha ao rodar o script de ffmpeg no python!");
+		return false; 
+	}
+	Print("[BIRDOAPP] movie criado " + output_file);
+	return true;
 }
 
 /*
@@ -570,24 +564,22 @@ extract audio from movie file
 @movie - arquivo de video de origem
 @outputFile - saida do arquivo em audio
 */
-function BD1_extract_audio_from_movieFile(birdoAppRoot, movie, outputFile){//converte o mov para audio (dar nome completo do output file com extencao);
-	var ffmpeg = BD1_Get_ffmpeg(birdoAppRoot);
-	
-	if(!BD1_FileExists(ffmpeg)){
-		Print("Erro: ffmpeg nao encontrado neste computador!");
-		return false;
-	}
-
-	var process = new Process2(ffmpeg, "-i", BD2_FormatPathOS(movie), outputFile);
-	var ret = process.launch();// let's home this worked.
+function BD1_extract_audio_from_movieFile(birdoAppRoot, movie, output_file){
+	var pythonPath = BD2_FormatPathOS(birdoAppRoot + "venv/Scripts/python");
+	var pyFile = BD2_FormatPathOS(birdoAppRoot + "app/utils/harmony_ffmpeg.py");
+	var start = Process2(pythonPath, pyFile, "extract_audio", "-i", movie, "-o", output_file);
+	var ret = start.launch();
 
 	if(ret != 0){
-		Print("Erro ao extrair audio: " + movie);
-		return false;
-	} else {
-		Print("audio extracted: " + outputFile);
+		Print("[BIRDOAPP] falha ao rodar o script de ffmpeg no python!");
+		return false; 
+	}
+	if(BD1_FileExists(output_file)){
+		Print("[BIRDOAPP] movie criado " + output_file);
 		return true;
 	}
+	Print("[BIRDOAPP] erro ao gerar o movie: " + output_file);
+	return false;
 }
 
 
@@ -653,19 +645,6 @@ function BD1_ForceMakeDir(dirPath){
 		}		
 	}
 	return false;
-}
-
-//run python script to copy file to destiny with progressbar (copy bytes per bytes);
-function BD1_copy_file_with_pb(proj_data, src_file, dst_file, ask_override){
-	if(!ask_override){
-		ask_override = "override";
-	}
-	
-	var python = proj_data.birdoApp + "venv/Scripts/python.exe";
-	var pyFile = "app/utils/copy_file_with_pb.py";
-	var start = Process2(python, pyFile, src_file, dst_file, ask_override);
-	var ret = start.launch();
-	return ret == 0;
 }
 
 
