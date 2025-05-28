ed69239267a87f9173bdd031e3fb2fbbc2f093f7
Leonardo Bazilio <leobazao@hotmail.com>
2025-05-28 14:20:22 -0700

    adicionei uma funcionalidade nova no compact scene modo batch: agora gera um arquivo mov do animatic (se encontrado na cena) dentro da pasta frames do arquivo harmony

--- batch/BAT_CompactScene.js
+++ batch/BAT_CompactScene.js
@@ -29,6 +29,8 @@ function BAT_CompactScene(){
 	}
 	
 	try{
+		
+		//run compact scene
 		var compact_func = require(projectDATA.paths.birdoPackage + "utils/compact_version.js");
 		var nodes_data = compact_func.create_compact_file_list(false);
 		var nodes_data = compact_func.create_compact_file_list(false);
@@ -36,7 +38,7 @@ function BAT_CompactScene(){
 		var used_nodes_list = nodes_data["file_list"].map(function(item){ return item["full_path"]});
 		var delete_list = allfiles.filter(function(item){ return used_nodes_list.indexOf(item) == -1});
 
-		Print("deletando " + delete_list.length + " arquivos...");
+		Print("[BIRDOAPP - compact scene] deletando " + delete_list.length + " arquivos...");
 		var counter = 0;
 		delete_list.forEach(function(item){
 			if(BD1_is_file(item)){
@@ -48,8 +50,12 @@ function BAT_CompactScene(){
 			}
 		});
 		
+		//create animatic movie from node...
+		var create_animatic = require(projectDATA.paths.birdoPackage + "utils/create_animatic_movie.js").crate_animatic_movie();
+
+		
 	}catch(e){
 		Print(e);
 	}
-	Print("END BATCH UPDATE SETUP!");
+	Print("[BIRDOAPP] END BATCH COMPACT SCENE!");
 }
---
+++ harmony/birdoPack/utils/create_animatic_movie.js
@@ -0,0 +1,68 @@
+/*
+	Este script roda a atualização do SETUP em modo batch
+*/
+include("BD_1-ScriptLIB_File.js");
+include("BD_2-ScriptLIB_Geral.js");
+
+
+function crate_animatic_movie(){
+		
+	var projectDATA = BD2_ProjectInfo();
+	if(!projectDATA){
+		Print("[BIRDOAPP - crate animatic movie][ERROR] Fail to get BirdoProject paths and data... canceling!");
+		return;
+	}
+	
+	var animatic = findAnimaticNode();
+	if(!animatic){
+		Print("[BIRDOAPP - crate animatic movie]cant find animatic node...");
+		return;
+	}
+	
+	var node_folder = element.completeFolder(node.getElementId(animatic));
+	var img_seq = BD1_ListFiles(node_folder, "*.png");
+	
+	var temp_folder = projectDATA.createTempFolder("animatic_movie", true);
+	img_seq.forEach(function(item){
+		var full_path = node_folder + "/" + item;
+		var namesplit = item.split("-");
+		var index = namesplit[namesplit.length -1].split(".")[0];
+		var name = "temp_" + ("0000" + index).slice(-4) + ".png"; 
+		
+		if(!BD1_CopyFile(full_path, temp_folder + name)){
+			Print("[BIRDOAPP - crate animatic movie]Error copying img " + full_path);
+			return 
+		}
+	});
+	
+	//create movie	
+	var frames_dir = scene.currentProjectPath() + "/frames/";
+	BD1_CleanFolder(frames_dir);
+	var movie = frames_dir + "animatic.mov";
+	var audio = sound.getSoundtrackAll().path();
+	var imagePatern = temp_folder + "temp_%04d.png";
+	Print("[BIRDOAPP - crate animatic movie]audio: "+ audio);
+	Print("[BIRDOAPP - crate animatic movie]Pattern: " + imagePatern);
+	if(!BD1_MakeMovieFromImageSeq(projectDATA.birdoApp, imagePatern, scene.getFrameRate(), audio, movie)){
+		Print("[BIRDOAPP - crate animatic movie]Fail to create movie from animatic...");
+		return;
+	}
+	
+	Print("[BIRDOAPP - crate animatic movie]Movie animatic created: " + movie);
+	
+}
+exports.crate_animatic_movie = crate_animatic_movie;
+
+function findAnimaticNode(){
+	var animatic_g = node.subNodes(node.root()).filter(function(item){ return node.isGroup(item) && node.getName(item).toUpperCase().indexOf("ANIMATIC") != -1});
+	if(animatic_g.length != 1){
+		Print("[BIRDOAPP - crate animatic movie]More than 1 animatic node in setup..");
+		return false;
+	}
+	var anim_nodes = node.subNodes(animatic_g[0]).filter(function(item){ return node.type(item) == "READ"});
+	if(anim_nodes.length != 1){
+		Print("[BIRDOAPP - crate animatic movie]Invalid animatic nodes number!");
+		return false;
+	}
+	return anim_nodes[0];
+}	
