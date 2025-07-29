b67a4383b686e0c3f584a9117ed3dc6a74cb2391
Leonardo Bazilio <leobazao@hotmail.com>
2025-07-29 07:43:05 -0700

    adicionei a linha de encoding no inicio e removi umas linhas extras vazias no codigo

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from app.birdoapp import BirdoApp
 from app.birdoapp_tools import DevTools
 from PySide import QtGui
@@ -7,11 +8,12 @@ import os
 
 app_root = os.path.dirname(os.path.realpath(__file__))
 
-def get_last_release(main_app):
 
-    cmd = "powershell.exe {0}".format(os.path.join(main_app,"update.ps1"))
+def get_last_release(main_app):
+    cmd = "powershell.exe {0}".format(os.path.join(main_app, "update.ps1"))
     print(cmd)
     return os.system(cmd) if main_app is not None else 0
+
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='BirdoApp - 2.0')
@@ -32,7 +34,7 @@ if __name__ == "__main__":
         DevTools().show_version()
     else:
 
-        last_updated_file = os.path.join(app_root,"lastUpdated.txt")
+        last_updated_file = os.path.join(app_root, "lastUpdated.txt")
         last_updated = os.path.getmtime(last_updated_file) if os.path.exists(last_updated_file) else 0
         get_last_release(app_root)
         if os.path.exists(last_updated_file) and (last_updated != os.path.getmtime(last_updated_file)):
