bd4ef0446ae648b58356a39b111c8d7f39f5ac7c
Leonardo Bazilio <leobazao@hotmail.com>
2025-12-05 12:57:16 -0800

    melhorei o texto na descricao do arquivo

--- harmony/birdoPack/utils/ui_utils.js
+++ harmony/birdoPack/utils/ui_utils.js
@@ -5,7 +5,7 @@
 include("BD_1-ScriptLIB_File.js");
 include("BD_2-ScriptLIB_Geral.js");
 
-//ui signals filter per harmony version (24 is different on the way to connect this king of signals
+//ui signals filter per harmony version (24 is different on the way to connect this kind of signals
 var signals = {
 	"combo": about.getMajorVersion() < 24 ? "[\"currentIndexChanged(int)\"]" : "currentIndexChanged",
 	"spin": about.getMajorVersion() < 24 ? "[\"valueChanged(int)\"]" : "valueChanged",
